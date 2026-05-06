1d796dc47d54369265a089c477154ef9e79e0f1b
Mehedi <mehedi.interface@gmail.com>
2026-05-06 21:52:33 +0600

    [200~19-2 Who is the tallest? Find the max number in an array ~

---
+++ talles.js
@@ -0,0 +1,16 @@
+const height = [65, 66, 68, 72, 78, 60]; 
+
+function getMax(numbers) {
+    let max = numbers[0]; 
+    for (const num of numbers) {
+        
+        if (num > max) {
+            max = num; 
+        }
+        // console.log(num); 
+    }
+    return max; 
+}
+
+const max = getMax(height); 
+console.log(max)
