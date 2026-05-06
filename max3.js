6abcebafba9c828e5619f780b7d6d55910306fd3
Mehedi <mehedi.interface@gmail.com>
2026-05-06 21:27:56 +0600

    19-1 Can you find out who will get the Strawberry?

---
+++ max3.js
@@ -0,0 +1,15 @@
+const jim = 56; 
+const tim = 89; 
+const kim = 68; 
+
+if (jim > tim && jim > kim) {
+    console.log("Jim is the ultimate boss"); 
+} else if (tim > jim && tim > kim) {
+    console.log("Tim is the boss"); 
+} else {
+    console.log("Kim is the kardashians boss"); 
+}
+
+const max = Math.max(12, 1, 56, 6, 22, 90, 34); 
+console.log("Max is using Math.max", max)
+
